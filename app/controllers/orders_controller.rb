261c7d65b2b92fe997a8e1207a45b8e50753a2b4
Tech-A10 <tech-a10@Tech-A10s-MacBook-Air.local>
2017-11-22 15:49:24 +0000

    dropdown box and orders in users

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,7 +15,7 @@ class OrdersController < ApplicationController
   # GET /orders/new
   def new
     @order = Order.new
-    # @order = User.all
+    @users = User.all
   end
 
   # GET /orders/1/edit
